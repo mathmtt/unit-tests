05385bc60c92632bddc454b39edef92ee2026894
Matheus Motta <matheusmotta136@gmail.com>
2023-05-03 15:43:13 -0300

    Requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -18,11 +18,14 @@ const assert = require('assert')
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    assert.
+    assert.strictEqual(numbers([1, 2]), true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    assert.strictEqual(numbers([1, 2, '3', 4, 5]), false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    assert.strictEqual(numbers(['a', 2, 3]), false);
     // Escreva um teste em que a função recebe [' '] e retorna false
+    assert.strictEqual(numbers([' ']), false);
   });
 });
