d6abf67fca24903bb9bf07de1b3e138859aa8308
Matheus Motta <matheusmotta136@gmail.com>
2023-05-04 16:59:34 -0300

    requisito 10incompleto

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,21 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const createMenu = () => {};
+const createMenu = (object) => {
+    const menu = {
+        fetchMenu: () => object,
+        consumption: [],
+        order: (string) => menu.consumption.push(string),
+        pay: () => {
+            const produtos = menu.consumption;
+            for (let index = 0; index < produtos.length; index += 1) {
+                if (produtos[index] === 'coxinha' || produtos[index] === 'agua') {
+                    return check(produtos);
+                }
+            }
+        }
+    }
+    return menu;
+};
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -2,7 +2,23 @@ const createMenu = require('../src/restaurant');
  
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // Escreva todos os testes aqui.
+    // Teste 01
+   expect(createMenu()).toHaveProperty('fetchMenu');
+   expect(typeof createMenu().fetchMenu).toBe('function');
   });
-});
+    // Teste 02
+   it('Teste 2: createMenu({ food: {}, drinks: {} }).fetchMenu() retorna um objeto cujas chaves são somente food e drinks', () => {
+    expect(createMenu({ food: {}, drink: {} }).fetchMenu()).toEqual({ food: {}, drink: {}});
+   });
+   // Teste 03
+   it('Teste 3', () => {
+    expect(createMenu({food: {coxinha: 3.90, sanduiche: 9.90}, drinks: {agua: 3.90, cerveja: 6.90}}).fetchMenu()).toEqual({food: {coxinha: 3.90, sanduiche: 9.90}, drinks: {agua: 3.90, cerveja: 6.90}});
+   });
+   it('Teste 5', () => {
+    expect(createMenu({food: {coxinha: 3.90, sanduiche: 9.90}, drinks: {agua: 3.90, cerveja: 6.90}})).toHaveProperty('consumption', [])
+   })
+   // Teste 7 
+  //  it('Teste 7', () => {
+  //   expect(createMenu()).order.toThrow(new Error("Item Indisponível"))
+  //  })
+ });
